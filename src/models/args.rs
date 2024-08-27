b99c76eefbea8ef682e51b2e65d050ee6e9e2ca2
Your Name <you@example.com>
2024-08-27 08:01:39 +0000

    Updated cli args

--- src/models/args.rs
+++ src/models/args.rs
@@ -8,22 +8,28 @@ pub struct Args {
     /// Torrent Api Url
     #[arg(short, long, default_value="some_url", env)]
     pub torrent_api_url: String,
-    /// Torrent Api Url
+
+    /// Transmission IP Address
     #[arg(long, default_value="127.0.0.1", env)]
     pub transmission_ipv4: String,
-    /// Torrent Api Url
+
+    /// Torrent Socks5 Proxy
     #[arg(long, default_value="127.0.0.1:9050", env)]
     pub tor_proxy_addr: String,
-    /// Torrent Api Url
+
+    /// Media Library Path
     #[arg(short, long, default_value="/media", env)]
     pub media_library: String,
-    /// Torrent Api Url
+
+    /// Debug Search Response - Path to Json file as response
     #[arg(short, long, env)]
     pub debug_search_response: Option<String>,
+
     /// Download speed limit kbps
-    #[arg(short, long, env)]
+    #[arg(short('s'), long, env)]
     pub speed_limit_down: Option<i32>,
-    /// Remove torrent after complete
+    
+    /// Script to run after torrent has complete
     #[arg(short('f'), long, env)]
     pub script_torrent_done_filename: Option<String>
 }
